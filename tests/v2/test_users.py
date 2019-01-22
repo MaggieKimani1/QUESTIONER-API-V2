c5c8721e52ea36ac7a65ae79cde53a28663eea68
Maggie Kimani <maggiekim42@gmail.com>
2019-01-22 09:55:09 +0300

    [Chore #163265903] Write test for user login

--- tests/v2/test_users.py
+++ tests/v2/test_users.py
@@ -10,3 +10,11 @@ class TestUsers(TestBase):
         expected = "user created"
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json.get('message'), expected)
+
+    def test_login(self):
+        self.client.post(
+            'api/v2/auth', data=json.dumps(self.correct_user), content_type="application/json")
+        response = self.client.post(
+            'api/v2/auth/login', data=json.dumps(self.login_user), content_type='application/json')
+        token = json.loads(response.get_data())["token"]
+        return token
