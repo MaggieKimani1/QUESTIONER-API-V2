be83111d64d5f984d2173ced10cf2ee112eb0e38
Maggie Kimani <maggiekim42@gmail.com>
2019-01-23 10:22:44 +0300

    [Feature #163266481] Creating an endpoint for posting a meetup

---
+++ app/api/v2/views/meetups.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource
+from app.api.v2.models.meetupmodel import Meetups
+from flask_expects_json import expects_json
+from app.api.v2.utils.json_schema import meetup_schema
+
+meetup = Meetups()
+
+
+class AllMeetupsApi(Resource):
+    """Endpoint for all meetups functionality"""
+
+    @expects_json(meetup_schema)
+    def post(self):
+        """This endpoint creates a meetup record"""
+        data = request.get_json()
+
+        if not data:
+            return {"message": "Please provide the required details", "status": 400}, 400
+
+        location = data["location"]
+        topic = data["topic"]
+        happeningOn = data["happeningOn"]
+        tags = data["tags"]
+
+        if not location or location.isspace():
+            return {"message": "location must be provided", "status": 400}, 400
+        if not topic or topic.isspace():
+            return {"message": "topic must be provided", "status": 400}, 400
+        if not happeningOn or happeningOn.isspace():
+            return {"message": "happeningOn must be provided", "status": 400}, 400
+        if not tags:
+            return {"message": "tags must be provided", "status": 400}, 400
+
+        if meetup.check_meetup(topic):
+            return {"message": "meetup already exists", "status": 400}, 400
+
+        meetup_record = meetup.create_meetup(
+            location, topic, happeningOn, tags)
+        if meetup_record:
+            return {"status": 201, "data": meetup_record, "message": "Meetup posted sucessfully"}, 201
+        return {"message": "Meetup failed to post"}, 400
