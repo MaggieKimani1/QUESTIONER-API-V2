9107be60f4c6c2923dc697220c748b7e73edff43
Maggie Kimani <maggiekim42@gmail.com>
2019-01-23 15:02:35 +0300

    [Feature #163266624] Endpoint for posting a question

---
+++ app/api/v2/models/questionsmodel.py
@@ -0,0 +1,29 @@
+from manage import Database
+from psycopg2.extras import RealDictCursor
+from instance.config import app_config
+import psycopg2
+import os
+import datetime
+
+db = Database()
+environment = os.environ["APP_SETTINGS"]
+DATABASE_URL = app_config[environment].DATABASE_URL
+
+
+class Questions():
+    """This class holds data for all questions"""
+
+    def __init__(self):
+        self.connection = psycopg2.connect(DATABASE_URL)
+        self.cursor = self.connection.cursor(cursor_factory=RealDictCursor)
+
+    def create_question(self, createdBy=None, meetup=None, title=None, body=None, upvotes=None, downvotes=None):
+        """Model for posting a meetup"""
+        createdOn = datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+
+        self.cursor.execute("INSERT INTO questions (createdOn, createdBy, meetup, title, body, upvotes, downvotes) VALUES(%s,%s,%s,%s,%s,%s,%s)",
+                            (createdOn, createdBy, meetup, title, body, upvotes, downvotes))
+
+        self.connection.commit()
+
+        return {"message": "question added!"}
