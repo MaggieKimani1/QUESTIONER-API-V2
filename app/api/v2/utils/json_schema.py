92b03ca81a15739bf7326cd415e84e49e6f72d15
Maggie Kimani <maggiekim42@gmail.com>
2019-01-23 10:23:42 +0300

    [Chore #163266481] Add validation for posting a meetup

---
+++ app/api/v2/utils/json_schema.py
@@ -0,0 +1,43 @@
+signup_schema = {
+    "type": "object",
+    "properties": {
+        "firstname": {type: "string"},
+        "lastname": {type: "string"},
+        "email": {type: "string"},
+        "password": {type: "string"},
+        "username": {type: "string"},
+        "phoneNumber": {type: "string"},
+        "registered": {type: "string"}
+    },
+    "required": ["firstname", "lastname", "email", "password", "username", "phoneNumber"]
+}
+
+login_schema = {
+    "type": "object",
+    "properties": {
+            "email": {type: "string"},
+        "password": {type: "string"}
+    },
+    "required": ["email", "password"]
+}
+meetup_schema = {
+    "type": "object",
+    "properties": {
+        "location": {type: "string"},
+        "topic": {type: "string"},
+        "happeningOn": {type: "string"},
+        "tags": {type: "string"},
+    },
+    "required": ["location", "topic", "happeningOn", "tags"]
+}
+
+question_schema = {
+    "type": "object",
+    "properties": {
+        "title": {type: "string"},
+        "body": {type: "string"},
+        "upvotes": {type: "string"},
+        "downvotes": {type: "string"},
+    },
+    "required": ["title", "body", "upvotes", "downvotes"]
+}
